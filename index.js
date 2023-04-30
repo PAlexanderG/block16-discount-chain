8387ffe19bbc5afb39c054b5d25595946058539f
Pedro Alexander Garcia A <pedroalexandergarciaa@imac.myfiosgateway.com>
2023-04-30 19:22:19 -0400

    pseudocode

---
+++ index.js
@@ -0,0 +1,36 @@
+//* Overview:
+// As you are picking out your froyo flavor at your favorite local frozen yogurt shop,
+// you overhear the management team discussing how they want to gather data to improve
+// customer retention. You make small-talk and offer your services to help them to build a survey.
+
+// Variables tables
+const Timmy = {
+  prescription: "acetaminaphen",
+  pricePerRefill: 25,
+  refills: 3,
+  subsciption: false,
+  coupon: true,
+};
+
+const sarah = {
+  prescription: "diphenhydramine",
+  pricePerRefill: 50,
+  refills: 1,
+  subscription: true,
+  coupon: false,
+};
+
+const rocky = {
+  prescription: "phenylephrine",
+  pricePerRefill: 30,
+  refills: 5,
+  subscription: true,
+  coupon: true,
+};
+
+function applyDiscount(custumer) {
+  // If a customer has a subcription the receive a 25% discount after the refill total has beeb calculated
+  // prices per refill multiply by refills to get discount
+  // if custumer have subcription then multiply by 0.75
+  // multiply total by 0.75
+}
